189e78ca94f449ce6ac76f8840f42fc640ddc5d7
Pooja Pasawala <poojapasawala@gmail.com>
2020-04-20 22:45:25 -0700

    Remove logo, App contents, and css. Add basic Nav component.

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Nav () {
+  return (
+    <nav className='nav'>
+      <ul>
+        <li>Home</li>
+        <li>New Question</li>
+        <li>Leaderboard</li>
+      </ul>
+    </nav>
+  )
+}
