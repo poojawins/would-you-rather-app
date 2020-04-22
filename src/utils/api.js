9b9864895d44d63fa725ad1941e089da872f250b
Pooja Pasawala <poojapasawala@gmail.com>
2020-04-22 00:01:52 -0700

    Add saveQuestion and saveQuestionAnswer to api.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,8 @@
 import {
   _getUsers,
   _getQuestions,
+  _saveQuestion,
+  _saveQuestionAnswer,
 } from './_DATA.js'
 
 export function getInitialData () {
@@ -11,4 +13,12 @@ export function getInitialData () {
     users,
     questions
   }))
+}
+
+export function saveQuestion(question) {
+  return _saveQuestion(question)
+}
+
+export function saveQuestionAnswer(info) {
+  return _saveQuestionAnswer(info)
 }
