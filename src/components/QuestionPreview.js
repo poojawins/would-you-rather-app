4e2a5c1d673ecdf42a966485dd3c8faa6c999ec4
Pooja Pasawala <poojapasawala@gmail.com>
2020-04-22 22:41:58 -0700

    QuestionPreview links to question view page.

--- src/components/QuestionPreview.js
+++ src/components/QuestionPreview.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Button, Card } from 'react-bootstrap'
 
-export default function QuestionPreview (props) {
+export default function QuestionPreview ({ question }) {
   return (
     <Card className='question-preview'>
-      <Card.Header>{props.question.author} asks:</Card.Header>
+      <Card.Header>{question.author} asks:</Card.Header>
       <Card.Body>
         <h3>Would you rather</h3>
-        <p>{props.question.optionOne.text} or...</p>
+        <p>{question.optionOne.text} or...</p>
       </Card.Body>
-      <Button>Answer</Button>
+      <Button href={`/questions/${question.id}`}>Answer</Button>
     </Card>
   )
 }
